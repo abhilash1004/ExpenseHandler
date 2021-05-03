8f3833974db422a6ddd77ba47b3ae4e301ba303d
Hrithik2001 <hrithikmallapuram2@gmail.com>
2021-05-03 12:39:20 +0530

    Login/register page UI change

--- app/src/main/java/com/mad_lab_project/expense_handler/MainActivity.java
+++ app/src/main/java/com/mad_lab_project/expense_handler/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import com.mad_lab_project.expense_handler.activities.Login_Activity;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
 
         Intent loginPageIntent = new Intent(this, Login_Activity.class);
